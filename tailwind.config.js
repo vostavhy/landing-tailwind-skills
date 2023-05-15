debbe3647291e74a2876d404933014ccce4a5077
vostavhy <vostavhy@gmail.com>
2023-05-15 22:57:45 +0200

    feat: implement header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      lg: "1980px",
+      md: "992px",
+      sm: "768px",
+      xs: "480px",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      backgroundImage: {
+        heroGradient:
+          "linear-gradient(94.59deg, #4923B4 2.39%, #E878CF 97.66%)",
+        logo: "url('/images/logo.svg')",
+      },
+      colors: {
+        primary: "#5027B5",
+        secondary: "#666768",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
